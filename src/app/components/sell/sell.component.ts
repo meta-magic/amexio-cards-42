30d254894cbcf644d0dbc364a87da77868869e2d
ashwiniagre1 <ashwini.agre@metamagic.in>
2019-02-22 19:43:30 +0530

    sell comp changes added

--- src/app/components/sell/sell.component.ts
+++ src/app/components/sell/sell.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DatatransferService } from "../../service/datatransfer.service";
 import { SearchModel } from "../../models/search.model";
+import { HttpService } from "src/app/service/shared/http.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { SellDataModel } from "../../models/sellData.model";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-sell',
@@ -8,15 +12,88 @@ import { SearchModel } from "../../models/search.model";
   styleUrls: ['./sell.component.css']
 })
 export class SellComponent implements OnInit {
-  searchModel:SearchModel;
+  searchModel: SearchModel;
+  sellModel: SellDataModel;
+  data: any;
+  categories: any;
+  sellFormGroup: FormGroup;
+  constImgaePath:string="assets/images/"
+  constructor(public _route: Router, public _dtsService: DatatransferService, public _httpService: HttpService, private fb: FormBuilder) {
+    this.searchModel = new SearchModel();
+    this.sellModel = new SellDataModel();
+    this.searchModel.type = 'sell';
+    this._dtsService.transparentFlag = false;
 
-  constructor(public dtsService:DatatransferService ) {
-     this.searchModel=new SearchModel();
-    this.searchModel.type='sell';
-    this.dtsService.transparentFlag=true;
-   }
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this._httpService.fetchdata('assets/json/sell.json').subscribe(
+      (res: any) => {
+        this.data = res;
+      });
+    this.valiadteSellForm();
+    this.categories = [{
+      id: 'tv',
+      name: 'TV'
+    },
+    {
+      id: 'apartment',
+      name: 'Apartment'
+    },
+   {
+      id: 'twoWheeler',
+      name: 'Two Wheeler'
+    },
+    {
+      id: 'electronics',
+      name: 'Electronics'
+    }];
+
+
+  }
+  valiadteSellForm() {
+    this.sellFormGroup = this.fb.group({
+      Title: ['', [Validators.required]],
+      SubTitle: ['', [Validators.required]],
+      Category: ['', [Validators.required]],
+      Tag: ['', [Validators.required]],
+      Price: ['', [Validators.required]],
+      City: ['', [Validators.required]],
+      Locality: ['', [Validators.required]],
+      Description: ['', [Validators.required]],
+      Photo: ['', { standalone: true }]
+    });
+  }
+  onPhotoUpload(event: any) {
+    this.sellModel.images=[];
+    console.log('photoUpload', event);
+    this.sellModel.image=this.constImgaePath+event[0].name;
+    console.log('img', this.sellModel.image)
+    // this.sellModel.images=event;
+    event.forEach((obj:any) => {
+    this.sellModel.images.push(this.constImgaePath+obj.name);
+    });
+    console.log('imgess',this.sellModel.images);
+  
+  }
+  onPhotoRemoveClick(event:any){
+    debugger;
+   console.log('remove', event);
+     this.sellModel.images.forEach((obj,index) => {
+       if(obj==this.constImgaePath+event.fileData.name){
+            this.sellModel.images.splice(index,1);
+       }
+    });
+    console.log('imgess1111',this.sellModel.images);
+  }
+  onBack() {
+    this._route.navigate(['/home'])
+  }
+  onAddClick(){
+    this.data.properties.push(this.sellModel);
+    console.log('data',this.data);
+    console.log('model',this.sellModel);
+    
+  }
 }
---
+++ src/app/models/sellData.model.ts
@@ -0,0 +1,15 @@
+export class SellDataModel {
+  title:string='';
+  subtitle:string='';
+  category:string='';
+  tag:string='';
+  price:string='';
+  city:string='';
+  locality:string='';
+  description:string='';
+  image:string='';
+  images:any=[];
+  constructor() {
+  }
+}
+
